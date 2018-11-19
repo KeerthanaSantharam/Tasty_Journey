c3b642910d3fd050937dd6b19068a4efb32593ba
KeerthanaSantharam <keertha.santha@gmail.com>
2018-11-19 16:09:27 +0800

    logo size changed

--- docs/custom.js
+++ docs/custom.js
@@ -32,4 +32,20 @@ btn.on('click', function(e) {
   e.preventDefault();
   $('html, body').animate({scrollTop:0}, '300');
 });
+$(document).ready(function() { function toggleSidebar() {$(".button").toggleClass("active");
+    $("main").toggleClass("move-to-left");
+    $(".sidebar-item").toggleClass("active");
+  }
+
+  $(".button").on("click tap", function() {
+    toggleSidebar();
+  });
+
+  $(document).keyup(function(e) {
+    if (e.keyCode === 27) {
+      toggleSidebar();
+    }
+  });
+
+});
 
